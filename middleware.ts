1b75eac55d256a4021e75333fabc67adaa202fd4
Anastasia Huz <anastasiahuz@Anastasias-MacBook-Air.local>
2026-04-11 19:09:34 +0300

    fix: proper middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export const runtime = 'nodejs'
-
 export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
