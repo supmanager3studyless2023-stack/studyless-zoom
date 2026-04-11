807d107721634ecde16b561f0efd73a9f7c69200
Anastasia Huz <anastasiahuz@Anastasias-MacBook-Air.local>
2026-04-11 18:51:36 +0300

    fix: set nodejs runtime for middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+export const runtime = 'nodejs'
+
 export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
